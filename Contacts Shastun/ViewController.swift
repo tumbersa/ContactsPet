2d784787b99e26707d5b7b293fcf1ad7d3ed8c55
tumbersa <gleb.kapustin.95@gmail.com>
2023-12-29 16:08:40 +0300

    Добавлено хранение через UserDefaults

--- Contacts Shastun/ViewController.swift
+++ Contacts Shastun/ViewController.swift
@@ -10,25 +10,61 @@ import UIKit
 fileprivate let reuseIdentifierCell = "contactCellIdentifier"
 
 class ViewController: UIViewController {
-    private var contacts = [ContactProtocol]()
+    var storage: ContactStorageProtocol!
+    
+    private var contacts: [ContactProtocol] = [] {
+        didSet {
+            contacts.sort { $0.title < $1.title }
+            storage.save(contacts: contacts)
+        }
+    }
 
+    @IBOutlet var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        storage = ContactStorage()
         
         loadContacts()
     }
 
     private func loadContacts() { 
-        contacts.append(
-    Contact(title: "Саня Техосмотр", phone: "+799912312323")) 
-        contacts.append(
-    Contact(title: "Владимир Анатольевич", phone: "+781213342321")) 
-        contacts.append(
-    Contact(title: "Сильвестр", phone: "+7000911112")) 
-        contacts.sort{ $0.title < $1.title }
+        contacts = storage.load()
     }
 
+    @IBAction func showNewContactAlert(){
+        let alertController = UIAlertController(
+            title: "Создайте новый контакт",
+            message: "Введите имя и телефон",
+            preferredStyle: .alert)
+            
+        alertController.addTextField { textField in
+            textField.placeholder = "Имя"
+        }
+        
+        alertController.addTextField { textField in
+            textField.placeholder = "Номер телефона"
+        }
+        
+        let createAction = UIAlertAction(title: "Создать",
+                                         style: .default) { _ in
+            guard let contactName = alertController.textFields?[0].text,
+                  let contactPhone = alertController.textFields?[1].text else {
+                return
+            }
+            
+            let contact = Contact(title: contactName, phone: contactPhone)
+            self.contacts.append(contact)
+            self.tableView.reloadData()
+        }
+        
+        let cancelButton = UIAlertAction(title: "Отменить", style: .cancel, handler: nil)
+        
+        alertController.addAction(createAction)
+        alertController.addAction(cancelButton)
+        
+        present(alertController, animated: true)
+    }
 }
 
 extension ViewController: UITableViewDataSource {
